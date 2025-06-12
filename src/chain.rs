358f65a0365cccad174fabc13e9ce1de96650725
Luke Lorenzini <luke.lorenzini@gmail.com>
2025-06-12 19:07:56 -0400

    Add chain processing

--- src/chain.rs
+++ src/chain.rs
@@ -1,13 +1,10 @@
-use hex::encode;
-use sha2::{Digest, Sha256};
-
 const VERSION: u32 = 0;
-const TRANSACTIONS_PER_BLOCK: u32 = 10;
+pub (crate) const TRANSACTIONS_PER_BLOCK: u32 = 10;
 
-struct Header {
+pub (crate) struct Header {
     version: u32,
     previous_block_hash: String,
-    merkle_tree_root: String,
+    pub (crate) merkle_tree_root: String,
     _timestamp: u64,
     _difficulty: u32,
     _nonce: u32,
@@ -41,14 +38,14 @@ impl Header {
     }
 }
 
-struct Block {
-    header: Header,
-    transaction_count: u32,
+pub struct Block {
+    pub (crate) header: Header,
+    pub (crate) transaction_count: u32,
     _transactions: u32,
 }
 
 impl Block {
-    fn new(previous_block_hash: String, merkle_tree_root: String) -> Self {
+    pub fn new(previous_block_hash: String, merkle_tree_root: String) -> Self {
         let header = Header::new(previous_block_hash, merkle_tree_root);
         Self { header, transaction_count: TRANSACTIONS_PER_BLOCK, _transactions: u32::default() }
     }
@@ -62,13 +59,13 @@ impl Block {
     }
 }
 
-struct Chain {
+pub struct Chain {
     blocks: Vec<Block>,
-    count: u32,
+    pub (crate) count: u32,
 }
 
 impl Chain {
-    fn new() -> Self {
+    pub fn new() -> Self {
         // let header = Header::genesis();
         // let genesis_block = Block::genesis();
         let genesis_block = vec![Block::genesis()];
@@ -78,34 +75,11 @@ impl Chain {
         }
     }
 
-    fn add_next_block(&mut self, block: Block) -> bool {
+    pub fn add_next_block(&mut self, block: Block) -> bool {
         self.blocks.push(block);
         self.count = self.blocks.len() as u32;
         true
     }
-}
-
-fn process_transaction(transaction: &i32) -> [u8; 4] {
-    transaction.to_be_bytes()
-}
-
-fn process_transactions(transactions: &[i32]) -> String {
-    let mut hasher = Sha256::new();
-    for transaction in transactions {
-        println!("{transaction:?}");
-        let tx = process_transaction(transaction);
-        hasher.update(tx);
-    }
-    // transactions.iter().map(|tx| process_transaction(tx)).for_each(|bytes| hasher.update(bytes));
-    let res = hasher.finalize();
-    let merkle_tree_root = encode(res);
-    println!("{merkle_tree_root:?}");
-    merkle_tree_root
-}
-
-fn create_new_block(previous_block_hash: String, transactions: &[i32]) -> Block {
-    let merkle_tree_root = process_transactions(transactions);
-    Block::new(previous_block_hash, merkle_tree_root)
 }
 
 #[cfg(test)]
@@ -134,36 +108,5 @@ mod test {
         let chain = Chain::new();
         let expected = 1;
         assert_eq!(chain.count, expected)
-    }
-
-    #[test]
-    fn test_add_new_block_to_chain() {
-        let mut chain = Chain::new();
-        let previous_block_hash = "0".into();
-        let transaction_pool = create_transaction_pool();
-        let transactions = &transaction_pool[0..TRANSACTIONS_PER_BLOCK as usize];
-        let block = create_new_block(previous_block_hash, transactions);
-        chain.add_next_block(block);
-        assert_eq!(chain.count, 2);
-    }
-
-    #[test]
-    fn test_process_transactions() {
-        let transaction_pool = create_transaction_pool();
-        let transactions = &transaction_pool[0..TRANSACTIONS_PER_BLOCK as usize];
-        let res = process_transactions(transactions);
-        let expected = "2c34ce1df23b838c5abf2a7f6437cca3d3067ed509ff25f11df6b11b582b51eb";
-        assert_eq!(res, expected);
-    }
-
-    #[test]
-    fn test_create_new_block() {
-        let previous_block_hash = "0".into();
-        let transaction_pool = create_transaction_pool();
-        let transactions = &transaction_pool[0..TRANSACTIONS_PER_BLOCK as usize];
-        let res = create_new_block(previous_block_hash, transactions);
-        let expected = "2c34ce1df23b838c5abf2a7f6437cca3d3067ed509ff25f11df6b11b582b51eb";
-        assert_eq!(res.header.merkle_tree_root, expected);
-        assert_eq!(res.transaction_count, TRANSACTIONS_PER_BLOCK);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+use hex::encode;
+use sha2::{Digest, Sha256};
 use serde_json::Result;
+
+use crate::chain::Block;
 
 mod chain;
 mod bank;
@@ -16,8 +20,40 @@ pub fn parse<T: Thing>(program: T, payload: &str) -> Result<()> {
     Ok(())
 }
 
+fn process_transaction(transaction: &i32) -> [u8; 4] {
+    // maybe call parse here?
+    transaction.to_be_bytes()
+}
+
+fn process_transactions(transactions: &[i32]) -> String {
+    let mut hasher = Sha256::new();
+    for transaction in transactions {
+        println!("{transaction:?}");
+        let tx = process_transaction(transaction);
+        hasher.update(tx);
+    }
+    // transactions.iter().map(|tx| process_transaction(tx)).for_each(|bytes| hasher.update(bytes));
+    let res = hasher.finalize();
+    let merkle_tree_root = encode(res);
+    println!("{merkle_tree_root:?}");
+    merkle_tree_root
+}
+
+pub fn create_new_block(previous_block_hash: String, transactions: &[i32]) -> Block {
+    let merkle_tree_root = process_transactions(transactions);
+    Block::new(previous_block_hash, merkle_tree_root)
+}
+
 #[cfg(test)]
 mod test {
+    use super::*;
+
+    use crate::chain::{Chain, TRANSACTIONS_PER_BLOCK};
+
+    fn create_transaction_pool() -> Vec<i32> {
+        vec![0; 100]
+    }
+
     fn _setup() -> String {
         r#"
         {
@@ -32,4 +68,35 @@ mod test {
     // fn test_parse() {
     //     let _res = parse();
     // }
+
+    #[test]
+    fn test_add_new_block_to_chain() {
+        let mut chain = Chain::new();
+        let previous_block_hash = "0".into();
+        let transaction_pool = create_transaction_pool();
+        let transactions = &transaction_pool[0..TRANSACTIONS_PER_BLOCK as usize];
+        let block = create_new_block(previous_block_hash, transactions);
+        chain.add_next_block(block);
+        assert_eq!(chain.count, 2);
+    }
+
+    #[test]
+    fn test_process_transactions() {
+        let transaction_pool = create_transaction_pool();
+        let transactions = &transaction_pool[0..TRANSACTIONS_PER_BLOCK as usize];
+        let res = process_transactions(transactions);
+        let expected = "2c34ce1df23b838c5abf2a7f6437cca3d3067ed509ff25f11df6b11b582b51eb";
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_create_new_block() {
+        let previous_block_hash = "0".into();
+        let transaction_pool = create_transaction_pool();
+        let transactions = &transaction_pool[0..TRANSACTIONS_PER_BLOCK as usize];
+        let res = create_new_block(previous_block_hash, transactions);
+        let expected = "2c34ce1df23b838c5abf2a7f6437cca3d3067ed509ff25f11df6b11b582b51eb";
+        assert_eq!(res.header.merkle_tree_root, expected);
+        assert_eq!(res.transaction_count, TRANSACTIONS_PER_BLOCK);
+    }
 }
