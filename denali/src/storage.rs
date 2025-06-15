c79232172b0bd85cc5903da0e63f59b2355585c9
Luke Lorenzini <luke.lorenzini@gmail.com>
2025-06-15 18:15:09 -0400

    Add storage skeleton

---
+++ denali/src/storage.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct State(HashMap<String, String>);
+
+impl State {
+    pub fn new() -> Self {
+        let mut inner = HashMap::default();
+        // Write our program address for now
+        inner.insert("fake_program".into(), "fake_program".into());
+        State { 0: inner }
+    }
+
+    fn _exists(&self, address: String) -> bool {
+        self.0.contains_key(&address)
+    }
+
+    fn _get_value(&self, key: &str) {
+        let _value = self.0.get(key);
+    }
+
+    fn _set_value() {}
+
+    fn _new_key_value() {}
+}
