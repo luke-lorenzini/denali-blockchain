b2012e5c1a9efea998074caaf9ad1edfbe559c78
Luke Lorenzini <luke.lorenzini@gmail.com>
2025-06-16 13:01:23 -0400

    Add set_value function

--- denali/src/storage.rs
+++ denali/src/storage.rs
@@ -1,14 +1,14 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, str::FromStr};
 
 #[derive(Clone, Debug)]
 pub struct State(HashMap<String, String>);
 
 impl State {
     pub fn new() -> Self {
-        let mut inner = HashMap::default();
+        let mut inner = HashMap::new();
         // Write our program address for now
         inner.insert("fake_program".into(), "fake_program".into());
-        State { 0: inner }
+        State (inner)
     }
 
     fn _exists(&self, address: String) -> bool {
@@ -19,7 +19,14 @@ impl State {
         let _value = self.0.get(key);
     }
 
-    fn _set_value() {}
+    pub fn set_value(&mut self, key: &str, value: &str) {
+        if self.0.contains_key(key) {
+            let val = self.0.get_mut(key).expect("Already checked");
+            *val = String::from_str(value).unwrap();
+        } else {
+            self.0.insert(key.into(), value.into());
+        }
+    }
 
-    fn _new_key_value() {}
+    pub fn new_key_value() {}
 }
