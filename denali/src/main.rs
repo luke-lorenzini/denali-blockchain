bf9d4b4be308b9af031963b7b82d3d4c7b1d4ed3
Luke Lorenzini <luke.lorenzini@gmail.com>
2025-06-16 12:02:16 -0400

    Remove logic from main

--- denali/src/main.rs
+++ denali/src/main.rs
@@ -1,12 +1,12 @@
-use denali::{
+// use denali::{
     // process_transactions,
-    Message,
-    Transactor,
+    // Message,
+    // Transactor,
     // chain::Chain,
     // create_new_block,
-    types::Thing,
-    storage::State,
-};
+    // types::Thing,
+    // storage::State,
+// };
 use tokio::{
     join, spawn, time::{sleep, Duration}
 };
@@ -15,31 +15,31 @@ use tokio::{
 async fn main() {
     println!("Hello, denali");
 
-    let mut transactor = Transactor::new();
-    let mut transactions = Vec::new();
+    // let mut transactor = Transactor::new();
+    // let mut transactions = Vec::new();
 
     // this comes from user input
-    let program = "fake_program";
-    let payload = r#"
-    {
-        "fake": 0
-    }"#
-    .into();
-    // end user input
-    match program {
-        "fake_program" => {
-            // Found a match with the vote program address
-            let fake = FakeProgram;
-            let message = Message {
-                payload,
-                program: fake,
-            };
-            transactions.push(message);
-        }
-        _ => {}
-    }
+    // let program = "fake_program";
+    // let payload = r#"
+    // {
+    //     "fake": 0
+    // }"#
+    // .into();
+    // // end user input
+    // match program {
+    //     "fake_program" => {
+    //         // Found a match with the vote program address
+    //         let fake = FakeProgram;
+    //         let message = Message {
+    //             payload,
+    //             program: fake,
+    //         };
+    //         transactions.push(message);
+    //     }
+    //     _ => {}
+    // }
 
-    let _res = transactor.create_new_block(transactions);
+    // let _res = transactor.create_new_block(transactions);
 
     let xxx = spawn(async move {
         println!("other thread");
@@ -53,21 +53,21 @@ async fn main() {
     let _res = join!(xxx);
 }
 
-#[derive(Clone)]
-struct FakeProgram;
+// #[derive(Clone)]
+// struct FakeProgram;
 
-impl Thing for FakeProgram {
-    fn run(&self, _payload: &str, state: &State) -> serde_json::Result<()> {
-        println!("run");
+// impl Thing for FakeProgram {
+//     fn run(&self, payload: &str, state: &mut State) -> serde_json::Result<()> {
+//         println!("run");
 
-        state.get_value("test");
+//         state.get_value("test");
 
         
-        Ok(())
-    }
+//         Ok(())
+//     }
 
-    fn verify(&self) -> serde_json::Result<bool> {
-        println!("verify");
-        Ok(true)
-    }
-}
+//     fn verify(&self) -> serde_json::Result<bool> {
+//         println!("verify");
+//         Ok(true)
+//     }
+// }
