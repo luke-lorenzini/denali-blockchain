7dbffa31b537a11b9312f8bc79c27120a2048449
Luke Lorenzini <luke.lorenzini@gmail.com>
2025-06-17 11:03:49 -0400

    Update integration tests

--- denali/tests/integration_tests.rs
+++ denali/tests/integration_tests.rs
@@ -72,6 +72,81 @@ fn test_modify_single_value() {
     assert_eq!(res, true)
 }
 
+#[test]
+fn test_add_one_block() {
+    let mut transactor = Transactor::new();
+    let mut transactions = Vec::new();
+    let fake = FakeProgram;
+    let payload = r#"
+    {
+        "fake": "1"
+    }"#
+    .into();
+    let message = Message {
+        payload,
+        program: fake,
+    };
+    transactions.push(message);
+    let res = transactor.create_new_block(transactions);
+    assert_eq!(res, true);
+    let res = transactor.get_chain_height();
+    let expected = 2;
+    assert_eq!(res, expected)
+}
+
+#[test]
+fn test_add_multiple_blocks() {
+    let mut transactor = Transactor::new();
+    let mut transactions = Vec::new();
+    let fake = FakeProgram;
+
+    let payload = r#"
+    {
+        "fake": "1"
+    }"#
+    .into();
+    let message = Message {
+        payload,
+        program: fake.clone(),
+    };
+    transactions.push(message);
+    let res = transactor.create_new_block(transactions.clone());
+    transactions.clear();
+    assert_eq!(res, true);
+
+    let payload = r#"
+    {
+        "fake": "2"
+    }"#
+    .into();
+    let message = Message {
+        payload,
+        program: fake.clone(),
+    };
+    transactions.push(message);
+    let res = transactor.create_new_block(transactions.clone());
+    transactions.clear();
+    assert_eq!(res, true);
+
+    let payload = r#"
+    {
+        "fake": "3"
+    }"#
+    .into();
+    let message = Message {
+        payload,
+        program: fake,
+    };
+    transactions.push(message);
+    let res = transactor.create_new_block(transactions.clone());
+    transactions.clear();
+    assert_eq!(res, true);
+
+    let res = transactor.get_chain_height();
+    let expected = 4;
+    assert_eq!(res, expected)
+}
+
 #[derive(Debug, Deserialize)]
 struct Payload {
     fake: String,
