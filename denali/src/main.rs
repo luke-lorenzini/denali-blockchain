a9dcb9bd59bf70bef0669d13aca35a35ee85bfce
Luke Lorenzini <luke.lorenzini@gmail.com>
2025-06-17 10:38:31 -0400

    Add main.rs

--- denali/src/main.rs
+++ denali/src/main.rs
@@ -1,73 +1,76 @@
-// use denali::{
-// process_transactions,
-// Message,
-// Transactor,
-// chain::Chain,
-// create_new_block,
-// types::Thing,
-// storage::State,
-// };
+use denali::{Message, Transactor, storage::State, types::Thing};
 use tokio::{
     join, spawn,
+    sync::mpsc::channel,
     time::{Duration, sleep},
 };
+
+const BATCH_SIZE: usize = 10;
 
 #[tokio::main]
 async fn main() {
     println!("Hello, denali");
 
-    // let mut transactor = Transactor::new();
-    // let mut transactions = Vec::new();
+    let (tx, mut rx) = channel(100);
+    let (tx_msg_queue, mut rx_msg_queue) = channel(100);
 
-    // this comes from user input
-    // let program = "fake_program";
-    // let payload = r#"
-    // {
-    //     "fake": 0
-    // }"#
-    // .into();
-    // // end user input
-    // match program {
-    //     "fake_program" => {
-    //         // Found a match with the vote program address
-    //         let fake = FakeProgram;
-    //         let message = Message {
-    //             payload,
-    //             program: fake,
-    //         };
-    //         transactions.push(message);
-    //     }
-    //     _ => {}
-    // }
-
-    // let _res = transactor.create_new_block(transactions);
-
-    let xxx = spawn(async move {
-        println!("other thread");
-
+    let listener_thread = spawn(async move {
         loop {
-            sleep(Duration::from_millis(1000)).await;
-            println!("awoken");
+            sleep(Duration::from_millis(100)).await;
+
+            let payload = r#"
+            {
+                "fake": 0
+            }"#
+            .into();
+            let fake = FakeProgram;
+            let message = Message {
+                payload,
+                program: fake,
+            };
+
+            tx.send(message).await.unwrap();
         }
     });
 
-    let _res = join!(xxx);
+    let receiver_thread = spawn({
+        async move {
+            let mut transactions = Vec::new();
+
+            while let Some(i) = rx.recv().await {
+                transactions.push(i);
+                if transactions.len() == BATCH_SIZE {
+                    tx_msg_queue.send(transactions.clone()).await.unwrap();
+                    transactions.clear();
+                }
+            }
+        }
+    });
+
+    let processor_thread = spawn(async move {
+        let mut transactor = Transactor::new();
+        println!("notified");
+
+        while let Some(messages) = rx_msg_queue.recv().await {
+            let _res = transactor.create_new_block(messages);
+        }
+    });
+
+    let _res = join!(receiver_thread, listener_thread, processor_thread);
 }
 
-// #[derive(Clone)]
-// struct FakeProgram;
+#[derive(Clone)]
+struct FakeProgram;
 
-// impl Thing for FakeProgram {
-//     fn run(&self, payload: &str, state: &mut State) -> serde_json::Result<()> {
-//         println!("run");
+impl Thing for FakeProgram {
+    fn run(&self, _payload: &str, state: &mut State) -> serde_json::Result<()> {
+        println!("run");
+        state.get_value("test");
+        Ok(())
+    }
 
-//         state.get_value("test");
-
-//         Ok(())
-//     }
-
-//     fn verify(&self) -> serde_json::Result<bool> {
-//         println!("verify");
-//         Ok(true)
-//     }
-// }
+    fn verify(&self) -> serde_json::Result<bool> {
+        println!("verify");
+        Ok(true)
+    }
+}
